47fdd133dca3f4ffb8c925296f2e268e4f8ebc3b
n3m3sis42 <n3m3sis42@gmail.com>
2017-08-08 10:36:19 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  good() {
+    console.log('Good!');
+  }
+
+  bad() {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return (
+      <button type="button" onFocus={this.good} onBlur={this.bad}>
+        Click Me
+      </button>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+
+  statusMsg = () => {
+    console.log('Entering password...');
+  }
+
+  render() {
+
+    return (
+      <input type="password" onKeyUp={this.statusMsg}/>
+    );
+  }
+
+}
+
+export default Keypad;
